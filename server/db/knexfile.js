b8ead74f4cd499bb6769b98bdae0ee5cbd02bbe4
mattsnz <matthewjsearle@gmail.com>
2022-04-30 08:10:29 +1200

    try this

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -41,7 +41,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      connectionString: process.env.HEROKU_POSTGRESQL_GOLD_URL,
       ssl: { rejectUnauthorized: false }
     },
     pool: {
