032e641468fc2e31a41a2781a330a964c807647b
mattsnz <matthewjsearle@gmail.com>
2022-04-30 08:40:35 +1200

    fuuuuuck me this is painful

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -39,9 +39,9 @@ module.exports = {
     }
   },
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      connectionString: process.env.HEROKU_POSTGRESQL_GOLD_URL,
       ssl: { rejectUnauthorized: false }
     },
     pool: {
